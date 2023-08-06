4aa67dc9ad7fbee5ecedf9823abd0497b50c3541
Abraham015 <ahernadeza1607@alumno.ipn.mx>
2023-08-06 15:31:51 -0600

    avance en el curso de go

---
+++ go/slices.go
@@ -0,0 +1,9 @@
+package main
+
+import "fmt"
+
+func main(){
+	//Un slice es una estructura dinamica, como sabemos
+	//los arreglos son de un tamaño definido y no se
+	//puede modificar dicho tamaño 
+}
